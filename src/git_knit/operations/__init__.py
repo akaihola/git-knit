3e39e9f83a06f5ec49f2ec1f13e9f5f59db0fe50
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2026-01-06 22:38:03 +0200

    feat: #6 - Extract GitSpiceDetector and KnitRebuilder into separate modules

--- src/git_knit/operations/__init__.py
+++ src/git_knit/operations/__init__.py
@@ -1,4 +1,6 @@
 from git_knit.operations.config import KnitConfig, KnitConfigManager
 from git_knit.operations.config import *  # noqa: F403, F401
 
-from .executor import GitExecutor, GitSpiceDetector, KnitRebuilder
+from .executor import GitExecutor
+from .spice_detector import GitSpiceDetector
+from .rebuilder import KnitRebuilder
